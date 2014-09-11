bff2502095d17d78dec8355d592c80d2a6743847
Simon A. F. Lund <safl@safl.dk>
2014-09-11 07:52:08 -0700

    Added 'chpl-init.h' to headers.

--- runtime/include/stdchpl.h
+++ runtime/include/stdchpl.h
@@ -49,7 +49,7 @@
 #include "chplio.h"
 #include "chplisdir.h"
 #include "chplmath.h"
-#include "chpl-main.h"
+#include "chpl-init.h"
 #include "chpl-mem.h"
 #include "chplmemtrack.h"
 #include "chpl-prefetch.h"
